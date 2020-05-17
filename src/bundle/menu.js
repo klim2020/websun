56fa625b5d025175513ded5d9220cbf9af02ef47
catcherochek <catcherochek@yandex.ru>
2020-05-18 00:27:42 +0300

    websunquiz

---
+++ src/bundle/menu.js
@@ -0,0 +1,29 @@
+$(window).on('load',function () {
+
+
+//jQuery to collapse the navbar on scroll
+   $(window).scroll(function () {
+        console.log( $(this).scrollTop() );
+        if ( $(this).scrollTop() > 150  ){
+
+            $(".navbar").addClass("scrolled").addClass("bounceInDown");
+
+        } else if($(this).scrollTop() <= 100) {
+
+            $(".navbar").removeClass("scrolled");
+
+        }
+
+    });
+
+
+    $('.mobile-menu').slicknav({
+        prependTo: '.menuplaceholder',
+        parentTag: 'liner',
+        allowParentLinks: true,
+        //duplicate: true,
+        //label: '',
+        //closedSymbol: '<i class="fa fa-chevron-right"></i>',
+        //openedSymbol: '<i class="fa fa-chevron-down"></i>',
+    });
+});
